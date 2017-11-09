cbe050d3fa1eb4e8f759e2b372dea1f9ca03fe74
shaheenz <shaheenz@hotmail.co.uk>
2017-11-09 21:37:26 +0000

    add first js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+console.log("Page has loaded! I can't believe it!");
+
+  function helloWorld() {
+
+    console.log('Hello 2nd World!');
+}
+
+function buttonThree() {
+  document.getElementById('heading-two').innerHTML = 'Hello World!';
+  console.log('button two pressed');
+}
